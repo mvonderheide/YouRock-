99e7ac25425644d51d013e8eebb5a5a6d84af8d0
Matthew Vonderheide <mattvonderheide@yahoo.com>
2021-04-18 20:30:33 -0500

    Initial commit

---
+++ YouRock!/Data1.swift
@@ -0,0 +1,20 @@
+//
+//  Data1.swift
+//  YouRock!
+//
+//  Created by admin on 4/16/21.
+//
+
+import SwiftUI
+
+struct Data1: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Data1_Previews: PreviewProvider {
+    static var previews: some View {
+        Data1()
+    }
+}
---
+++ YouRock!/Period1.swift
@@ -0,0 +1,20 @@
+//
+//  Period1.swift
+//  YouRock!
+//
+//  Created by admin on 4/16/21.
+//
+
+import SwiftUI
+
+struct Period1: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Period1_Previews: PreviewProvider {
+    static var previews: some View {
+        Period1()
+    }
+}
